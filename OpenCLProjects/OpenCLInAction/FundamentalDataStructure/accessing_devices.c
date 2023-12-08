64ad3b25950a51996d47bb262086e7c47396799b
Hung Pham <phamhung20022015@gmail.com>
2023-12-08 09:59:38 +0000

    feat(OpenCLInAction): accessing platforms and devices script

---
+++ OpenCLProjects/OpenCLInAction/FundamentalDataStructure/accessing_devices.c
@@ -0,0 +1,62 @@
+#include <stdio.h> 
+
+#ifdef MAC
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+int main() {   
+  // Identify platform
+  cl_platform_id *platforms;
+  cl_uint num_entries = 2, err = CL_SUCCESS, num_platforms;
+  // The first run to identify how many number of platforms available
+  err = clGetPlatformIDs(num_entries, NULL, &num_platforms);
+  if (err < 0) {
+    perror("Couldn't find any platforms");
+    exit(1);
+  } else {
+    printf("Num platforms: %d\n", num_platforms);
+  }
+  // Memory allocation for platform based on # of platforms above
+  platforms = (cl_platform_id *) malloc(sizeof(cl_platform_id) * num_platforms);
+  // In here you pass num_platforms to num_entries because you want to know all IDs.
+  clGetPlatformIDs(num_platforms, platforms, NULL);
+
+  // Identify devices id
+  cl_device_id *devices;
+  cl_uint num_devices;
+  // The first time run to determine how many number of device avaiable in platform
+  // we only need the # of device so we set our expectation(# of entries = 0).
+  err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 0, NULL, &num_devices);
+  if (err < 0) {
+    perror("Couldn't find any devices");
+    exit(1);
+  } else {
+    printf("Num devices: %d\n", num_devices);
+  }
+  // Then we need to allocate devices
+  devices = (cl_device_id *) malloc(sizeof(cl_device_id) * num_devices);
+  clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, num_devices, devices, NULL);
+
+  // Identify devices info
+  cl_device_type device_type;
+  cl_uint device_vendor_id, device_max_compute_units;
+  char device_extensions[1024];
+  for (int i = 0; i < num_devices; ++i) {
+      clGetDeviceInfo(devices[i], CL_DEVICE_TYPE, sizeof(device_type), &device_type, NULL);
+      clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR_ID, sizeof(device_vendor_id), &device_vendor_id, NULL);
+      clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(device_max_compute_units), &device_max_compute_units, NULL);
+      clGetDeviceInfo(devices[i], CL_DEVICE_EXTENSIONS, sizeof(device_extensions), &device_extensions , NULL);
+      // Using & not && in here because device_type is long unsigned int
+      // and CL_DEVICE_TYPE_GPU != 0 so if use && in here the condition always
+      // true -> wrong. In here we have 2 number so we need bitwise & operator.
+      if (device_type & CL_DEVICE_TYPE_GPU) {
+        printf("Device type: GPU\n");
+      }
+      printf("Device vendor id: %d\n", device_vendor_id);
+      printf("Device max_compute_units: %d\n", device_max_compute_units);
+      printf("Device extension(s): %s\n", device_extensions);
+  }
+
+}
---
+++ OpenCLProjects/OpenCLInAction/FundamentalDataStructure/accessing_platforms.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+
+#ifdef MAC
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+int main() {   
+  // Identify platform id
+  cl_platform_id *platforms;
+  cl_uint err = 0, num_platforms;
+  // The first parameter in here is num_entries, it's only meaningful after determining
+  // the number of platform first, so it will be set to 0.
+  // platforms at least = 0 because logic, you should add at least 0 entries to platforms
+  err = clGetPlatformIDs(0, NULL, &num_platforms);
+  if (err < 0) {
+    perror("Couldn't find any platforms");
+    exit(1);
+  } else {
+    printf("Found %d platform(s)\n", num_platforms);
+  }
+  // Because you can run script in many type of platforms, therefore, good practice
+  // is allocate memory based on previous # platform, if not script will return 
+  // segmentfault.
+  platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * num_platforms);
+  // Now your platforms is determined, you wish to get ID of all platform so pass
+  // num_platforms to the first parameter of clGetPlatformIDs as num_entries.
+  clGetPlatformIDs(num_platforms, platforms, NULL);
+
+  // Identify multi platform info
+  char platform_name[100], platform_vendor[100], platform_profile[100], platform_version[100], platform_extensions[2048];
+  for (int i = 0; i < num_platforms; i++) {
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, sizeof(platform_name), platform_name, NULL);
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(platform_vendor), platform_vendor, NULL);
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, sizeof(platform_version), platform_version, NULL);
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, sizeof(platform_profile), platform_profile, NULL);
+    clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, sizeof(platform_extensions), platform_extensions, NULL);
+    printf("Platform name: %s\n", platform_name);
+    printf("Platform vendor: %s\n", platform_vendor);
+    printf("Platform version: %s\n", platform_version);
+    printf("Platform profile: %s\n", platform_profile);
+    printf("Platform extension: %s\n", platform_extensions);
+  }
+
+}
